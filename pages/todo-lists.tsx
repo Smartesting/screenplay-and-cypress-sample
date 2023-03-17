8e5ccfd1d249d92adaac4da09748f45bafc50f8b
Vincent Pretre <vincent.pretre@gmail.com>
2023-03-17 14:26:43 +0100

    Fix build

--- pages/todo-lists.tsx
+++ pages/todo-lists.tsx
@@ -5,13 +5,22 @@ import styles from "../styles/Home.module.css";
 import { HttpClient } from "../src/client/HttpClient";
 import { ToDoListCreationForm } from "../src/components/ToDoLists/CreateForm";
 import { ToDoLists } from "../src/components/ToDoLists/List";
+import { JwtUserIdentification } from "../src/types";
 
 const ToDoListComponent: React.FunctionComponent = () => {
   const client = new HttpClient("http://localhost:3000");
-  const userIdentification = JSON.parse(
-    window.sessionStorage.getItem("userIdentification") ?? "{}"
-  );
+  const [userIdentification, setUserIdentification] =
+    React.useState<JwtUserIdentification>();
 
+  React.useEffect(() => {
+    if (userIdentification) return;
+
+    setUserIdentification(
+      JSON.parse(window.sessionStorage.getItem("userIdentification") ?? "{}")
+    );
+  });
+
+  if (!userIdentification) return <></>;
   return (
     <>
       <ToDoListCreationForm
